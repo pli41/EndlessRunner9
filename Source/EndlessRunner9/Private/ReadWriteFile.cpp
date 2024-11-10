f8f0667baaddf692d48da944c602cf584b24ae1c
Peilin Li <peilinli524@gmail.com>
2024-11-10 00:06:54 -0800

    Implemented highscore sorting and highscore UI

--- Source/EndlessRunner9/Private/ReadWriteFile.cpp
+++ Source/EndlessRunner9/Private/ReadWriteFile.cpp
@@ -14,23 +14,46 @@ FString UReadWriteFile::ReadStringFromFile(FString FilePath, bool& bOutSuccess, 
 		return "";
 	}
 
-	FString ReturnString = "";
+	FString ReadString = "";
 	// Try reading the file
-	if (!FFileHelper::LoadFileToString(ReturnString, *FilePath))
+	if (!FFileHelper::LoadFileToString(ReadString, *FilePath))
 	{
 		bOutSuccess = false;
 		OutInfoMessage = FString::Printf(TEXT("Read String From File Failed - Was not able to read file"));
 		return "";
 	}
+	// We offset the characters by -20 to decode the message
+
+
+	// Split the array into 
+	TArray<FString> outStringArr;
+	ReadString.ParseIntoArray(outStringArr, LINE_TERMINATOR, true);
+
+	// Trim the strings before processing
+	for (FString str : outStringArr) {
+		str.TrimStartAndEndInline();
+	}
+
+	// Sort in Descending order
+	outStringArr.Sort([](FString A, FString B) { return FCString::Atoi(*A) > FCString::Atoi(*B); });
+
+	FString ResultString = "";
+	// Construct new high score string
+	for (FString str : outStringArr) {
+		ResultString += str + LINE_TERMINATOR;
+	}
+
 
 	bOutSuccess = true;
 	OutInfoMessage = FString::Printf(TEXT("Read String From File succeeded"));
-	return ReturnString;
+	return ResultString;
 }
 
 
 FString UReadWriteFile::WriteStringToFile(FString FilePath, FString String, bool& bOutSuccess, FString& OutInfoMessage)
 {
+	// We offset the characters in the strings by +20 to encode the message
+
 	// Try writting to file
 	if (!FFileHelper::SaveStringToFile(String, *FilePath))
 	{
