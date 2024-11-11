ef56251542e1978f3d036a634d0e6503c10f0a89
Peilin Li <peilinli524@gmail.com>
2024-11-10 16:01:18 -0800

    added ASCII offset to "encrypt" the highscore data, and optimized MainMenu and Pause Menu's read file logic

--- Source/EndlessRunner9/Private/ReadWriteFile.cpp
+++ Source/EndlessRunner9/Private/ReadWriteFile.cpp
@@ -22,24 +22,29 @@ FString UReadWriteFile::ReadStringFromFile(FString FilePath, bool& bOutSuccess, 
 		OutInfoMessage = FString::Printf(TEXT("Read String From File Failed - Was not able to read file"));
 		return "";
 	}
-	// We offset the characters by -20 to decode the message
-
-
 	// Split the array into 
 	TArray<FString> outStringArr;
 	ReadString.ParseIntoArray(outStringArr, LINE_TERMINATOR, true);
 
 	// Trim the strings before processing
-	for (FString str : outStringArr) {
-		str.TrimStartAndEndInline();
+	TArray<FString> newStringArr;
+
+	for (FString String : outStringArr) {
+		String.TrimStartAndEndInline();
+		// TODO: We offset the characters by -20 to decode the message
+		FString newString = "";
+		for (int i = 0; i < String.Len(); i++) {
+			newString += char(String[i] - 20);
+		}
+		newStringArr.Add(newString);
 	}
 
 	// Sort in Descending order
-	outStringArr.Sort([](FString A, FString B) { return FCString::Atoi(*A) > FCString::Atoi(*B); });
+	newStringArr.Sort([](FString A, FString B) { return FCString::Atoi(*A) > FCString::Atoi(*B); });
 
 	FString ResultString = "";
 	// Construct new high score string
-	for (FString str : outStringArr) {
+	for (FString str : newStringArr) {
 		ResultString += str + LINE_TERMINATOR;
 	}
 
@@ -52,7 +57,12 @@ FString UReadWriteFile::ReadStringFromFile(FString FilePath, bool& bOutSuccess, 
 
 FString UReadWriteFile::WriteStringToFile(FString FilePath, FString String, bool& bOutSuccess, FString& OutInfoMessage)
 {
-	// We offset the characters in the strings by +20 to encode the message
+	// TODO: We offset the characters in the string by +20 to encode the message
+	for (int i = 0; i < String.Len(); i++) {
+		if (String[i] != '\n' and String[i] != '\r') {
+			String[i] = String[i] + 20;
+		}
+	}
 
 	// Try writting to file
 	if (!FFileHelper::SaveStringToFile(String, *FilePath))
