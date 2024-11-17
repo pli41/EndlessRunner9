076e6a46a91984d2c6680caa226a9cc5c0c730be
Peilin Li <peilinli524@gmail.com>
2024-11-16 18:06:23 -0800

    cleanups

--- Source/EndlessRunner9/Private/ReadWriteFile.cpp
+++ Source/EndlessRunner9/Private/ReadWriteFile.cpp
@@ -22,32 +22,35 @@ FString UReadWriteFile::ReadStringFromFile(FString FilePath, bool& bOutSuccess, 
 		OutInfoMessage = FString::Printf(TEXT("Read String From File Failed - Was not able to read file"));
 		return "";
 	}
-	// Split the array into 
+	// Split the array into string
+	// ["EF", "EH"]
 	TArray<FString> outStringArr;
 	ReadString.ParseIntoArray(outStringArr, LINE_TERMINATOR, true);
 
-	// Trim the strings before processing
 	TArray<FString> newStringArr;
-
 	for (FString String : outStringArr) {
+		// Trim the strings before processing
 		String.TrimStartAndEndInline();
 		// TODO: We offset the characters by -20 to decode the message
 		FString newString = "";
 		for (int i = 0; i < String.Len(); i++) {
 			newString += char(String[i] - 20);
 		}
-		newStringArr.Add(newString);
+		newStringArr.Add(newString); // ["15", "17"]
 	}
 
 	// Sort in Descending order
+	// [15, 17]
+	// [17, 15]
 	newStringArr.Sort([](FString A, FString B) { return FCString::Atoi(*A) > FCString::Atoi(*B); });
 
+	// 17
+	// 15
 	FString ResultString = "";
 	// Construct new high score string
 	for (FString str : newStringArr) {
 		ResultString += str + LINE_TERMINATOR;
 	}
-
 
 	bOutSuccess = true;
 	OutInfoMessage = FString::Printf(TEXT("Read String From File succeeded"));
@@ -57,7 +60,7 @@ FString UReadWriteFile::ReadStringFromFile(FString FilePath, bool& bOutSuccess, 
 
 FString UReadWriteFile::WriteStringToFile(FString FilePath, FString String, bool& bOutSuccess, FString& OutInfoMessage)
 {
-	// TODO: We offset the characters in the string by +20 to encode the message
+	// We offset the characters in the string by +20 to encode the message
 	for (int i = 0; i < String.Len(); i++) {
 		if (String[i] != '\n' and String[i] != '\r') {
 			String[i] = String[i] + 20;
